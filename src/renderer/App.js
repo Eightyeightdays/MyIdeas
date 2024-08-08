9a652b6aeb11484e107016b1ec528e880bab5496
Eightyeightdays <eightyeightdays@gmail.com>
2024-08-08 13:15:05 +0200

    Nodes rendered correctly on page from DB

--- src/renderer/App.js
+++ src/renderer/App.js
@@ -1,79 +1,93 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import icon from '../../assets/icon.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import PouchDB from "pouchdb";
 import { ReactFlow, Background, Controls } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
 
 function Home() {
+    const DB_NAME = "MYIDEAS";
     const [dbName, setDbName] = useState();
     const [nodeName, setNodeName] = useState();
     const [notification, setNotification] = useState();
-   
-    function createDatabase(){
+    const [allDocs, setAllDocs] = useState();
+    const [nodeData, setNodeData] = useState();
+    const db = new PouchDB(DB_NAME);
+    const nodes = [];
     
-        console.log("WORKING")
-        const db = new PouchDB(dbName);
-        setNotification(`Database "${dbName}" Created Successfully`);
 
-        db.info().then(info => console.log(info))
-    };
 
-    function updateData(e){
-        setDbName(e.target.value);
-    };
+  
+  
+    useEffect(()=>{
+        const getDocs = async()=>{    
+            // try {
+            //     await db.destroy();
+            // } catch (err) {
+            //     console.log(err);
+            // }
+            // try{
+            //     const res = await db.bulkDocs([ 
+            //         {
+            //             id: '1',
+            //             data: { label: 'Hello' },
+            //             position: { x: 0, y: 0 },
+            //         },
+            //         {
+            //             id: '2',
+            //             data: { label: 'World' },
+            //             position: { x: 100, y: 100 },
+            //         },
+            //         {
+            //             id: '3',
+            //             data: { label: 'Goodbye' },
+            //             position: { x: 200, y: 200 },
+            //         },
+            //         {
+            //             id: '3',
+            //             data: { label: 'Planet Earth' },
+            //             position: { x: 300, y: 300 },
+            //         },
+            //     ]);
+            // }catch(err){
+            //     console.log(err);
+            // }
 
-    function handleNewNodeName(e){
-        setNodeName(e.target.value);
-    }
+            try{               
+                const result = await db.allDocs({
+                    include_docs: true
+                });
+                
+                const docs = result.rows.map(row => {
+                    row.doc.id = row.doc._id;
+                    delete row.doc._rev;
+                    delete row.doc._id;
+                    return row.doc;
+                });
 
-    function addNode(){
-        const doc = {
-            "_id": new Date(),
-            "data": nodeName
+                setNodeData(docs);
+
+                console.log(nodeData);
+            }catch(err){
+                console.log(err);
+            };
         };
         
-        const db = new PouchDB("crimson")
-        try{
-            db.put(doc);
-        }catch(err){
-           console.log(err);
-        }
-
-        db.info().then(info =>  setNotification(JSON.stringify(info)));
-        setNodeName("");
-    }
+        getDocs();
+    }, [db]);
 
   return (
     <div>
-      
       <h1>Ideation App</h1>
-        {!notification && <div>
-            <label htmlFor="dbName">Database name: </label>
-            <input id="dbName" type='text' value={dbName} onChange={updateData}></input>
-            <button name="dbName" onClick={createDatabase}>Create</button>
-        </div>}
-       
-        {notification && <p>{notification}</p>}
-        
-        <div>
-            <label htmlFor="addNode">Add a node: </label>
-            <input type="text" id="addNode" value={nodeName} onChange={handleNewNodeName}></input>
-            <button name="" onClick={addNode}>Add</button>
-        </div>
 
-        <div style={{ height: '500px', width: "500px" }}>
-            <ReactFlow>
+        {nodes && <div style={{ height: '500px', width: "500px" }}>
+            <ReactFlow nodes={nodeData}>
                 <Background />
                 <Controls />
             </ReactFlow>
-        </div>
-
-        <p>END</p>
-
+        </div>}
     </div>
-
   );
 }
 
